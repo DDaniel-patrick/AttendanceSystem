3d47175c4061e7a45a33fab237d8350e3c12ce0e
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2023-07-10 02:52:40 +0100

    Added Login

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,8 +1,51 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
 
-router.get('/', (req, res) => {
-    res.render('auth/login')
-})
+const registerMod = require("./../../models/user/auth/register");
 
-module.exports = router
+router.get("/", (req, res) => {
+  res.render("auth/login", { msg: "" });
+});
+
+router.post("/", async (req, res) => {
+  const sess = req.session;
+  const loginEmail = req.body.email;
+  const loginPassword = req.body.password;
+  console.log(loginEmail);
+  console.log(loginPassword);
+  try {
+    if (loginPassword.length >= 6) {
+      const check = await registerMod.findOne({ email: loginEmail });
+      if (check) {
+        const comparePass = bcrypt.compareSync(loginPassword, check.password);
+        if (comparePass == true) {
+          if (check.verified == true) {
+            (sess.email = loginEmail),
+              (sess.password = loginPassword),
+              (sess.identifier = "user");
+            console.log(sess);
+            res.redirect("/home");
+          } else {
+            sess.email = loginEmail;
+            sess.password = loginPassword;
+            res.redirect("/register/otp");
+          }
+        } else {
+          res.render("auth/login", { msg: "Incorrect Password" });
+        }
+      } else {
+        res.render("auth/login", { msg: "Incorrect Email" });
+      }
+    } else {
+      res.render("auth/login", {
+        msg: "Password must be 6 or more characters",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.render("auth/login", { msg: "An Error Occured!!!" });
+  }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index')
-})
+router.get("/", (req, res) => {
+  const sess = req.session;
+  if (sess.email && sess.password && sess.identifier === "user") {
+    res.render("index");
+  } else {
+    res.redirect("/");
+  }
+});
 
-module.exports = router
+module.exports = router;
