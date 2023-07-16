1121911d8dd2c0590b652d40d433ec09eeda740a
Solutech11 <soluwizy@gmail.com>
2023-07-16 19:00:32 +0100

    home update

--- routes/Dashboard/home.js
+++ routes/Dashboard/home.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const attendance = require('../../models/user/attendance/attendance')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    const sess = req.session
-    if(sess.email && sess.password && sess.identifier === 'user') {
-        res.render('Dashboard/home')
-    } else {
-        res.redirect('/')
+router.get('/', async (req, res) => {
+    try {
+        const sess = req.session
+        if(sess.email && sess.password && sess.identifier === 'user') {
+            let allAttendance = await attendance.find({adminId:sess?._id})
+            res.render('Dashboard/home',{allAttendance})
+
+        } else {
+            res.redirect('/')
+        }
+    } catch (error) {
+        res.status(500).render('500',{msg:Errordisplay(error).msg})
     }
+    
 })
 
 module.exports = router
