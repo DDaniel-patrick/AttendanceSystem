ec96614495b7eefb388f553f9cc33fd6966d6c7d
Solutech11 <soluwizy@gmail.com>
2023-07-20 02:06:26 +0100

    delete attendance done

--- routes/Dashboard/home.js
+++ routes/Dashboard/home.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const attendance = require('../../models/user/attendance/attendance')
+const attendanceRegistrers = require('../../models/user/attendance/attendanceRegistrers')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
@@ -9,6 +10,25 @@ router.get('/', async (req, res) => {
             let allAttendance = await attendance.find({adminId:sess?._id})
             res.render('Dashboard/home',{allAttendance})
 
+        } else {
+            res.redirect('/')
+        }
+    } catch (error) {
+        res.status(500).render('500',{msg:Errordisplay(error).msg})
+    }
+    
+})
+
+router.get('/delete', async (req, res) => {
+    try {
+        const sess = req.session
+
+        const AttendanceID= req.query.id
+        if(sess.email && sess.password && sess.identifier === 'user') {
+
+            AttendanceID?AttendanceID.length==24?await attendance.deleteOne({_id:AttendanceID, adminId:sess?._id}):null:null
+            AttendanceID?AttendanceID.length==24?await attendanceRegistrers.deleteMany({attendanceId:AttendanceID, adminId:sess?._id}):null:null
+            res.redirect('/')
         } else {
             res.redirect('/')
         }
